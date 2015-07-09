d3eadad95d49270713119d54a69aa93a38160432
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-07-09 20:36:48 +0900

    rough inmp d.cpp

--- atcoder09052015/src/d.cpp
+++ atcoder09052015/src/d.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+class vector_comp {
+public:
+	bool operator()(std::vector<int> a, std::vector<int> b){
+		return a[0] > b[0];
+	}
+};
 
 int main(int argc, char** argv){
-  std::cout << "argc=" << argc << ", argv=" << argv << std::endl;
+	int N;
+	std::vector<std::vector<int> > HS;
+	vector_comp vc;
+
+	int ret = 0;
+
+	std::cin >> N;
+	HS.resize(N, std::vector<int>(2));
+
+	for ( int i=0 ; i<N ; i++ ){
+		std::cin >> HS[i][0] >> HS[i][1];
+	}
+
+	// greedy search
+	for ( int i=0 ; i<N ; i++ ){
+		std::vector<std::vector<int> >::iterator it = std::min_element(HS.begin(), HS.end(), vc);
+
+		if ( ret < (*it)[0] ){
+			ret = (*it)[0];
+		}
+
+		HS.erase(it);
+
+		for ( int j=0 ; j<HS.size() ; j++ ){
+			HS[j][0] += HS[j][1];
+		}
+	}
+
+	std::cout << ret << std::endl;
 }
