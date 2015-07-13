c96da8df35d017a169ffed1e34499f34930247a2
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-07-14 01:55:10 +0900

    impl a/b,cpp

--- ARC040/src/a.cpp
+++ ARC040/src/a.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <vector>
 
-int main(int argc, char** argv){
-  std::cout << "argc=" << argc << ", argv=" << argv << std::endl;
+int main(int argc, char** argv) {
+	int N;
+	std::vector<std::string> S;
+	int score = 0;
+
+	std::cin >> N;
+
+	S.resize(N);
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> S[i];
+		for (int j = 0; j < N; j++) {
+			switch (S[i][j]) {
+			case 'R':
+				score++;
+				break;
+			case 'B':
+				score--;
+				break;
+			}
+		}
+	}
+
+	if (score > 0) {
+		std::cout << "TAKAHASHI" << std::endl;
+	} else if (score == 0) {
+		std::cout << "DRAW" << std::endl;
+	} else {
+		std::cout << "AOKI" << std::endl;
+	}
 }
--- ARC040/src/b.cpp
+++ ARC040/src/b.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv){
-  std::cout << "argc=" << argc << ", argv=" << argv << std::endl;
+	int N, R;
+	std::string S;
+	int ret = 0;
+	int pos = 0;
+	int last_dot_pos = -1;
+
+	std::cin >> N >> R >> S;
+
+	for ( int i=N-1 ; i>=0 ; i-- ){
+		if ( S[i] == '.' ){
+			last_dot_pos = i;
+			break;
+		}
+	}
+
+	if ( last_dot_pos < 0 ){
+		std::cout << "0" << std::endl;
+		return 0;
+	}
+
+	while ( pos < N ) {
+		// std::cout << S << " at " << pos << std::endl;
+		if (last_dot_pos - pos < R ){
+			ret++ ;
+			break;
+		} else if (S[pos] == '.'){
+			for ( int j=0 ; j<R ; j++ ){
+				S[pos+j] = 'o';
+			}
+		} else {
+			pos++;
+		}
+		ret++;
+	}
+
+	std::cout << ret << std::endl;
+	return 0;
 }
