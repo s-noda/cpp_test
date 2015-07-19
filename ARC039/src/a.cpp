bcdd57254a6304abe9d40df000b43f0fe5bfd1aa
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-07-19 18:03:48 +0900

    impl a.cpp

--- ARC039/src/a.cpp
+++ ARC039/src/a.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
 
-int main(int argc, char** argv){
-  int N;
-  std::cin >> N;
+int main(int argc, char** argv) {
+	int A, B;
+	std::vector<int> A_, B_;
+	std::vector<int> ret;
 
-  std::cout << N << std::endl;
+	std::cin >> A >> B;
+
+	ret.resize(6);
+	A_.resize(3);
+	B_.resize(3);
+
+	A_[2] = A % 10;
+	A_[1] = A % 100 - A_[2];
+	A_[0] = A % 1000 - A_[1] - A_[2];
+
+	B_[2] = B % 10;
+	B_[1] = B % 100 - B_[2];
+	B_[0] = B % 1000 - B_[1] - B_[2];
+
+//	std::cout << "A = ";
+//	for (int i = 0; i < A_.size(); i++)
+//		std::cout << A_[i] << ". ";
+//	std::cout << std::endl;
+//
+//	std::cout << "B = ";
+//	for (int i = 0; i < A_.size(); i++)
+//		std::cout << B_[i] << ". ";
+//	std::cout << std::endl;
+
+	ret[0] = (900 + A_[1] + A_[2]) - B;
+	ret[1] = (A_[0] + 90 + A_[2]) - B;
+	ret[2] = (A_[0] + A_[1] + 9) - B;
+	ret[3] = A - (100 + B_[1] + B_[2]);
+	ret[4] = A - (B_[0] + 0 + B_[2]);
+	ret[5] = A - (B_[0] + B_[1] + 0);
+
+	std::cout << (*std::max_element(ret.begin(), ret.end())) << std::endl;
 }
--- ARC039/src/b.cpp
+++ ARC039/src/b.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
+
+int comb(int N, int k){
+	if ( k == 1 ) return N;
+	else return N * comb(N-1,k-1) / k;
+}
+
+int comb2(int d, int k, std::vector<bool> &map){
+	if ( k == 1 ){
+		int ret = 0;
+		for ( int i=d ; i<map.size() ; i++ ){
+			if ( map[i] ){
+
+			} else {
+				ret = ( ret + 1 ) % 1000000007 ;
+			}
+		}
+		return ret;
+	} else {
+		int ret = 0;
+		for ( int i=d ; i<map.size() ; i++ ){
+			if ( map[i] ){
+			} else {
+				map[i] = true;
+				ret = ( ret + comb2(i+1,k-1,map) ) % 1000000007 ;
+				map[i] = false;
+			}
+		}
+		return ret;
+	}
+}
 
 int main(int argc, char** argv){
-  int N;
-  std::cin >> N;
+  int N, K;
+  int split;
 
-  std::cout << N << std::endl;
+  std::cin >> N >> K;
+
+  if ( K % N == 0 ){
+	  std::cout << 1 << std::endl;
+	  return 0;
+  }
+
+  split = K % N;
+
+  std::vector<bool> map;
+  map.resize(N, false);
+  std::cout << (comb2(0, split,map) %  1000000007) << std::endl;
 }
