c4970801742ea60784b7f027ba2bf0e0de628671
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2015-08-01 03:52:43 +0900

    impl a b .cpp

--- ARC038/src/a.cpp
+++ ARC038/src/a.cpp
@@ -5,7 +5,22 @@
 
 int main(int argc, char** argv){
   int N;
+  std::vector<int> score;
+  int ret = 0;
+
   std::cin >> N;
 
-  std::cout << N << std::endl;
+  score.resize(N);
+
+  for ( int i=0 ; i<N ; i++ ) std::cin >> score[i];
+
+  std::sort(score.begin(), score.end(), std::greater<int>());
+
+  for ( int i=0 ; i<N ; i++ ){
+	  if ( i % 2 == 0 ){
+		  ret += score[i];
+	  }
+  }
+
+  std::cout << ret << std::endl;
 }
--- ARC038/src/b.cpp
+++ ARC038/src/b.cpp
@@ -3,9 +3,53 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv){
-  int N;
-  std::cin >> N;
+bool check_map(int i, int j, std::vector<std::string> &map){
+	if ( i>=0 && j>=0 ){
+		map[i][j] = '#';
+	}
+	return ( i==0 && j==0 );
+}
 
-  std::cout << N << std::endl;
+int main(int argc, char** argv){
+  int H, W;
+  std::vector<std::string> map;
+
+  std::cin >> H >> W;
+
+  map.resize(H+1, std::string(W+1, '#'));
+  std::string buf;
+  for ( int i=0; i<H ; i++ ){
+	  std::cin >> buf;
+	  for ( int j=0 ; j<W ; j++ ){
+		  map[i][j] = buf[j];
+	  }
+	  //std::cout << map[i] << std::endl;
+  }
+
+  int cnt=0;
+  while ( true ){
+	  for ( int i=1 ; i<H+1 ; i++ ){
+		  for ( int j=1 ; j<W+1 ; j++ ){
+			  if ( map[i][j] == '#' && map[i-1][j] == '#' && map[i][j-1] == '#' && map[i-1][j-1] != '#'){
+				  if ( check_map(i-2, j-2, map) ||  check_map(i-2, j-1, map) ||  check_map(i-1, j-2, map) || check_map(i-1, j-1, map) ){
+					  goto RET;
+				  }
+			  }
+		  }
+	  }
+//	  for ( int i=0 ; i<map.size() ; i++ ) std::cout << map[i] << std::endl;
+//	  std::cout << std::endl;
+
+	  cnt++;
+  }
+  RET:
+  //if ( cnt % 2 == 1 ){
+	  if ( map[1][0] != '#' || map[0][1] != '#'|| map[1][1] != '#'){
+		  std::cout << "First" << std::endl;
+	  } else {
+		  std::cout << "Second" << std::endl;
+	  }
+//  }  else {
+//	  std::cout << "First" << std::endl;
+//  }
 }
